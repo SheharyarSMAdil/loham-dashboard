a1d4c2d0e9aad125c4cd6145b57e28c90392a625
unknown <sheharyarsmadil@outlook.com>
2022-09-08 13:06:09 +0530

    initial commit

---
+++ assets/script/api.js
@@ -0,0 +1,185 @@
+const electron = require("electron");
+const { ipcRenderer } = electron;
+
+ipcRenderer.on("pid:added", fetchID);
+ipcRenderer.on("pid:deleted", fetchID);
+
+const fetchServerData = () => {
+  allParaStructure = [];
+  let thresholdData = JSON.parse(localStorage.getItem("threshold")) || {};
+  const storedData = JSON.parse(localStorage.getItem("info")) || [];
+  document.getElementById("deviceWrapper").innerHTML = "";
+  // console.log(storedData);
+  // console.log(storedData.length)
+
+  // var dvTable = document.getElementById("dvTable");
+  // dvTable.innerHTML = "";
+
+  storedData.forEach((info) => {
+    console.log(info.pid);
+
+    var head = new Array();
+    head.push(["Parameter", "Value"]);
+
+    //Create a HTML Table element.
+    var table = document.createElement("table");
+    table.border = "1";
+
+    //Get the count of columns.
+    var columnCount = head[0].length;
+
+    //Add the header row.
+    var row = table.insertRow(-1);
+    var header = document.createElement("th");
+    header.colSpan = 2;
+    header.innerHTML = info.alias;
+    row.appendChild(header);
+
+    row = table.insertRow(-1);
+    let cardContent = "";
+    // const serverData = async () => {
+    fetch("https://api.enggenv.com/api/fetchdata?id=" + info.pid)
+      .then((response) => response.json())
+      .then((data) => {
+        delete data.lg;
+        delete data.lt;
+        delete data.bv;
+        delete data.ts_client;
+        delete data.ts_server;
+
+        console.log(data);
+        let deviceWrapper = document.createElement("div");
+        deviceWrapper.setAttribute("class", "deviceWrapper");
+        deviceWrapper.setAttribute("id", "deviceWrapper");
+        let paraList = [];
+        Object.entries(data).forEach((item) => {
+          paraList.push(item[0]);
+
+          let status = thresholdData[info.pid]
+            ? thresholdData[info.pid][item[0]] < item[1]
+              ? "invalid"
+              : "valid"
+            : "";
+          cardContent += `<div class="card" data-threshold=${status}>
+                      <div class="cardHeader">
+                          <p>${item[0].split("_").join(".")}</p>
+                      </div>
+                      <div class="cardBody">
+                          <p>${item[1]}</p>
+                      </div>
+                  </div>`;
+        });
+
+        allParaStructure.push({ id: info.pid, para: [...paraList] });
+        document.getElementById(
+          "deviceWrapper"
+        ).innerHTML += `<label><strong>${info.pid}</strong> (${info.alias})</label><div class="cardWrapper">  ${cardContent}</div>`;
+
+        // for (var i = 0; i < columnCount; i++) {
+        //     var headerCell = document.createElement("th");
+        //     headerCell.innerHTML = head[0][i];
+        //     row.appendChild(headerCell);
+        // }
+
+        // var keys = Object.keys(data);
+
+        // keys.forEach((para_val) => {
+        //     row = table.insertRow(-1);
+        //     var paraName = document.createElement("th");
+        //     paraName.innerHTML = para_val.toUpperCase();
+        //     row.appendChild(paraName);
+        //     var cell = row.insertCell(-1);
+        //     cell.innerHTML = data[para_val];
+        // });
+      })
+      .catch((err) => console.error(err));
+    // }
+
+    // var dvTable = document.getElementById("dvTable");
+    // dvTable.innerHTML += "";
+    // dvTable.appendChild(table);
+  });
+};
+
+const closeWindowHandler = () => {
+  document.getElementById("thresholdCard").style.display = "none";
+};
+
+const validateThresholdHandler = () => {
+  let id = document.getElementById("deviceIds").value;
+  let para = document.getElementById("devicePara").value;
+  let threshold = document.getElementById("thresholdValue").value;
+  let thresholdData = JSON.parse(localStorage.getItem("threshold")) || {};
+  //   thresholdData = [];
+  if (thresholdData[id]) {
+    thresholdData[id] = { ...thresholdData[id], [para]: threshold };
+  } else {
+    thresholdData = { ...thresholdData, [id]: { [para]: threshold } };
+  }
+  localStorage.setItem("threshold", JSON.stringify(thresholdData));
+};
+
+const showThresholdCard = () => {
+  document.getElementById("thresholdCard").style.display = "flex";
+  deviceList = JSON.parse(localStorage.getItem("info")).map(
+    (device) => device.pid
+  );
+  console.log(deviceList);
+  document.getElementById("deviceIds").innerHTML = "";
+  deviceList.forEach((item) => {
+    document.getElementById(
+      "deviceIds"
+    ).innerHTML += `<option value=${item}>${item}</option>`;
+  });
+  setParaHandler({ value: deviceList[0] });
+};
+
+const setParaHandler = (e) => {
+  document.getElementById("devicePara").innerHTML = "";
+  console.log(e.value);
+  console.log(allParaStructure);
+  let optionPara = allParaStructure.find(
+    (device) => device.id === e.value
+  ).para;
+  optionPara.forEach((item) => {
+    document.getElementById(
+      "devicePara"
+    ).innerHTML += `<option value=${item}>${item}</option>`;
+  });
+};
+
+document
+  .getElementById("downloadData")
+  .addEventListener("click", fetchServerData);
+document
+  .getElementById("setThreshold")
+  .addEventListener("click", showThresholdCard);
+
+function fetchID(e, status) {
+  console.log(status);
+  localStorage.setItem("status", status);
+  const info = document.getElementById("info");
+  info.innerHTML = localStorage.getItem("info");
+  // const li = document.createElement('li');
+  // const itemText = document.createTextNode(item);
+  // li.appendChild(itemText);
+  // ul.appendChild(li);
+}
+
+getInfo = () => {
+  document.getElementById("deviceWrapper").innerHTML = "";
+  if (window.navigator.onLine && JSON.parse(localStorage.getItem("info"))) {
+    fetchServerData();
+  } else {
+    if (window.navigator.onLine) {
+    }
+  }
+
+  setTimeout(() => {
+    getInfo();
+  }, 20000);
+};
+
+getInfo();
+
+var allParaStructure = [];
---
+++ main.js
@@ -0,0 +1,145 @@
+const electron = require('electron')
+const url = require('url')
+const path = require('path')
+
+// SET ENV
+process.env.NODE_ENV = 'development';
+process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = false;
+
+const { app, BrowserWindow, Menu, ipcMain } = electron
+
+let mainWindow, addWindow, deleteWindow
+
+// Listen for app to be ready
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        },
+        title: 'Online Dashboard'
+    })
+
+    // load html into window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file',
+        slashes: true
+    }))
+
+    // quit app when closed
+    mainWindow.on('close', () => {
+        app.quit()
+    })
+
+    // Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    // Insert Menu
+    Menu.setApplicationMenu(mainMenu);
+
+})
+
+// Handle create add window
+function createAddWindow() {
+    // create new window
+    addWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        },
+        width: 300,
+        height: 260,
+        title: 'Add Device ID'
+    });
+    // Load HTML into window
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addID.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    addWindow.setMenuBarVisibility(false)
+}
+
+// Handle create delete window
+function createDeleteWindow() {
+    // create new window
+    deleteWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        },
+        width: 400,
+        height: 420,
+        title: 'Delete Device ID'
+    });
+    // Load HTML into window
+    deleteWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'deleteID.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    deleteWindow.setMenuBarVisibility(false)
+}
+
+// Catch pid:added
+ipcMain.on('pid:added', function (e, status) {
+    mainWindow.webContents.send('pid:added', status);
+    addWindow.close();
+});
+
+// Catch pid:deleted
+ipcMain.on('pid:deleted', function (e, status) {
+    mainWindow.webContents.send('pid:deleted', status);
+    deleteWindow.close();
+});
+
+// Create menu template
+const mainMenuTemplate = [
+    {
+        label: 'Options',
+        submenu: [
+            {
+                label: 'Add Device ID',
+                click() {
+                    createAddWindow();
+                }
+            },
+            {
+                label: 'Delete Device ID',
+                click() {
+                    createDeleteWindow();
+                }
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' :
+                    'Ctrl+Q',
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
+
+// // Add developer tools item if not in production
+if (process.env.NODE_ENV !== 'production') {
+    mainMenuTemplate.push({
+        label: 'Developer Tools',
+        submenu: [
+            {
+                label: 'Toggle DevTools',
+                accelerator: process.platform == 'darwin' ? 'Command+Shift+I' :
+                    'Ctrl+Shift+I',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    });
+}
